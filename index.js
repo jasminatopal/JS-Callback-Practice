6498b715bd872a584b8e99c9815a31b4d4d14615
Hadeel Ammari <80487488+hadeelammari@users.noreply.github.com>
2023-01-29 12:44:18 -0800

    Update index.js

--- index.js
+++ index.js
@@ -26,53 +26,6 @@ function handleDirectionChange(direction){
 move(character).withArrowKeys(100, 250, handleDirectionChange)
 
 
-
-let direction = null;
-let x = 100;
-let y = 250;
-
-function moveCharacter(){ 
-    if(direction === 'west'){
-        x = x - 1
-    }
-    if(direction === 'north'){
-        y = y + 1
-    }
-    if(direction === 'east'){
-        x = x + 1
-    }
-    if(direction === 'south'){
-        y = y - 1
-    }
-    character.style.left = x + 'px'
-    character.style.bottom = y + 'px'
-}
-
-setInterval(moveCharacter, 1)                                    
-
-
-document.addEventListener('keydown', function(e){
-    if(e.repeat) return;
-
-    if(e.key === 'ArrowLeft'){
-        direction = 'west'
-    }
-    if(e.key === 'ArrowUp'){
-        direction = 'north'
-    }
-    if(e.key === 'ArrowRight'){
-        direction = 'east'
-    }
-    if(e.key === 'ArrowDown'){
-        direction = 'south'
-    }
-})
-
-document.addEventListener('keyup', function(e){
-    direction = null
-})
-
-
 move(newImage('assets/tree.png')).withArrowKeys(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
